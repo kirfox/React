cab27123476fc3031e74975530de5f83e03ae0fe
kirill <muxin.kirill2011@yande.ru>
2020-10-26 13:50:17 +0300

    feature: customize button

---
+++ src/Components/NavBar.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+import logoImg from '../image/logo.svg';
+
+const NavBarStyled = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  height: 80px;
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  background-color: #299B01;
+  color: white;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const H1 = styled.h1`
+  font-size: 24px;
+  margin-left: 15px;
+`;
+
+const ImgLogo = styled.img`
+  width: 50px;
+`;
+
+const Login = styled.button`
+  width: 100px;
+  border: 1px solid white;
+  background-color: transparent;
+  color: white;
+  font-size: 18px;
+  padding: 10px;
+  cursor: pointer;
+  transition: .3s linear;
+
+  :hover {
+    color: #299B01;
+    background-color: white;
+  }
+
+  :active, :focus {
+    outline: none;
+  }
+`;
+export const NavBar = () => (
+  <NavBarStyled>
+    <Logo>
+      <ImgLogo src={logoImg} alt='logo'/>
+      <H1>MrDonald's</H1>
+    </Logo>
+    <Login>войти</Login>
+  </NavBarStyled>
+)
+
+ 
